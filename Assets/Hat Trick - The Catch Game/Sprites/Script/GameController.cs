8c8bcaf7ed35af7cf0b09939e804ceef7442958d
Ashley Geary <472555@mymail.sussexdowns.ac.uk>
2017-12-08 11:27:31 +0000

    08/12/2017 Version 1.0

---
+++ Assets/Hat Trick - The Catch Game/Sprites/Script/GameController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour
+{
+
+    public Camera cam;
+    public GameObject ball;
+    public float timeLeft;
+    public Text timerText;
+    public GameObject gameOverText;
+    public GameObject restartButton;
+
+    private float maxWidth;
+
+    void Start()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+        Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0f);
+        Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+        float ballWidth = ball.GetComponent<Renderer>().bounds.extents.x;
+        maxWidth = targetWidth.x - ballWidth;
+        StartCoroutine(Spawn());
+
+    }
+    void FixedUpdate()
+    {
+        timeLeft -= Time.deltaTime;
+        if(timeLeft < 0){
+            timeLeft = 0;
+        }
+        timerText.text = "Time Left:\n" + Mathf.RoundToInt(timeLeft);
+
+    }
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(2.0f);
+        while (timeLeft > 0)
+        {
+            Vector3 spawnPosition = new Vector3(
+                Random.Range(-maxWidth, maxWidth),
+                transform.position.y,
+                0.0f
+                );
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(ball, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
+        }
+        yield return new WaitForSeconds(2.0f);
+        gameOverText.SetActive(true);
+        yield return new WaitForSeconds(2.0f);
+        restartButton.SetActive(true);
+
+    }
+}
+
+
---
+++ Assets/Restart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restart : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
