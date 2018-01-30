ad1377ed8831479931f97a6eef1c4378a7871f63
Ashley Geary <472555@mymail.sussexdowns.ac.uk>
2018-01-30 10:24:22 +0000

    no message

--- Assets/Hat Trick - The Catch Game/Script/DestroyPlayer.cs
+++ Assets/Hat Trick - The Catch Game/Script/DestroyPlayer.cs
@@ -2,17 +2,3 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class DestroyPlayer : MonoBehaviour
-//{
-//    GameObject player;
-
-//    private void start()
-//    {
-//        player = GameObject.Find("Hat");
-//    }
-
-//    void OnTriggerEnter2D(Collider2D collision)
-//    {
-//        Destroy(player);
-//    }
-//}
