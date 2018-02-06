d8463be442b025fce806b0c60ca585bc7920bbec
Ashley Geary <472555@mymail.sussexdowns.ac.uk>
2018-02-06 10:02:31 +0000

    Working Destroyers

--- Assets/Hat Trick - The Catch Game/Script/DestroyOnContact.cs
+++ Assets/Hat Trick - The Catch Game/Script/DestroyOnContact.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 public class DestroyOnContact : MonoBehaviour
 
 {
-    void OnTriggerExit(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         Destroy(gameObject);
+
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
